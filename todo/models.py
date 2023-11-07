97481634416103fed62ece0b617ace3082c951df
caketaster <caketaster@gmail.com>
2023-11-07 10:03:24 +0000

    Add form, change to template

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False) 
+    # ^ name cannot be blank, so blank=False
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # to change the default name...
+    def __str__(self):
+        return self.name
+

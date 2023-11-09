0af9f41011cb5b97dea5271e8a3c35d6e9a4b5a9
caketaster <caketaster@gmail.com>
2023-11-09 11:33:33 +0000

    Attempt to setup development environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,9 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = ['8000-caketaster-hellodjango-hq7msw355xy.ws-us105.gitpod.io','8000-caketaster-hellodjango-hq7msw355xy.ws-us106.gitpod.io', 'caketaster-todo-app-382e7d899e17.herokuapp.com/']
+if development:
+    ALLOWED_HOSTS = ['localhost', '8000-caketaster-hellodjango-hq7msw355xy.ws-us105.gitpod.io','8000-caketaster-hellodjango-hq7msw355xy.ws-us106.gitpod.io']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), 'caketaster-todo-app-382e7d899e17.herokuapp.com/']
 
 
 # Application definition
@@ -74,20 +79,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/ 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('postgres://zjskqxka:Xd5A1GZagO_RUiK_8pdZgLyKHQSIIm2R@rain.db.elephantsql.com/zjskqxka'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
+    }
+else: 
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('postgres://zjskqxka:Xd5A1GZagO_RUiK_8pdZgLyKHQSIIm2R@rain.db.elephantsql.com/zjskqxka'))
+    }
 
 
 # Password validation
